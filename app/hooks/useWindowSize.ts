3dcb4d04ac6b9a1628faf55aabcecfad0d44cd46
SirKenG <sirkeng.me@gmail.com>
2024-10-29 01:11:29 +0700

    feat: adapt swiper usage based on screen size for mobile and desktop
    - Added a responsive design for the  using Swiper on desktop while keeping a standard layout for mobile.
    - Integrated  hook to determine the current window width and conditionally render Swiper only on desktop devices.

---
+++ app/hooks/useWindowSize.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export default function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Set initial window size
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
--- app/sports-page/LastSeasonsSection.tsx
+++ app/sports-page/LastSeasonsSection.tsx
@@ -6,10 +6,12 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
+import useWindowSize from "../hooks/useWindowSize";
 
 export default function LastSeasonsSection() {
   const [lastSeasons, setLastSeasons] = useState([]);
   const [expandedSeasons, setExpandedSeasons] = useState({});
+  const { width } = useWindowSize();
 
   useEffect(() => {
     fetchLastSeasons();
@@ -53,19 +55,85 @@ export default function LastSeasonsSection() {
           <div className="my-2">
             <h4 className="blue-text text-center fs-1">RECENT POSTS</h4>
           </div>
-          <Swiper
-            navigation={true}
-            pagination={{
-              dynamicBullets: true,
-            }}
-            slidesPerView={3}
-            spaceBetween={30}
-            modules={[Pagination, Navigation]}
-            className="mySwiperLastSeasons"
-          >
-            {lastSeasons.map((season, index) => (
-              <SwiperSlide key={index}>
-                <div className="col-12 mb-4">
+          {width > 768 ? (
+            <Swiper
+              navigation={true}
+              pagination={{
+                dynamicBullets: true,
+              }}
+              slidesPerView={3}
+              spaceBetween={30}
+              modules={[Pagination, Navigation]}
+              className="mySwiperLastSeasons"
+            >
+              {lastSeasons.map((season, index) => (
+                <SwiperSlide key={index}>
+                  <div className="col-12 mb-4">
+                    <div
+                      className="card bg-grey shadow"
+                      style={{
+                        border: "none",
+                        width: "100%",
+                        maxWidth: "380px",
+                      }}
+                    >
+                      <motion.div
+                        initial={{ opacity: 0, y: "50px" }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ type: "Tween", stiffness: 100 }}
+                      >
+                        <div className="card">
+                          <Link href={`/season-sports?id=${season?.sport?.id}`}>
+                            <div className="card-header fs-4 text-center blue-text">
+                              {season.seasonNumber}
+                            </div>
+                            <Image
+                              height={300}
+                              width={500}
+                              src={`${process.env.NEXT_PUBLIC_API_BASE_URL}${season.seasonImageUrl}`}
+                              alt="Season Image"
+                              className="card-img-top card-img-top-radius-unset"
+                              style={{ objectFit: "cover", height: "300px" }}
+                            />
+                          </Link>
+                          <div className="card-body">
+                            <p className="card-text text-black text-roboto">
+                              {expandedSeasons[index] ||
+                              season.seasonDetail.length <= 100
+                                ? season.seasonDetail
+                                : `${season.seasonDetail.substring(0, 100)}...`}
+                              {season.seasonDetail.length > 100 &&
+                                !expandedSeasons[index] && (
+                                  <button
+                                    className="btn btn-link p-0 blue-text ms-1"
+                                    onClick={() => toggleSeeMore(index)}
+                                  >
+                                    See More
+                                  </button>
+                                )}
+                            </p>
+                            {expandedSeasons[index] &&
+                              season.seasonDetail.length > 100 && (
+                                <button
+                                  className="btn btn-link p-0 blue-text"
+                                  onClick={() => toggleSeeMore(index)}
+                                >
+                                  See Less
+                                </button>
+                              )}
+                          </div>
+                        </div>
+                      </motion.div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <div className="row">
+              {lastSeasons.map((season, index) => (
+                <div key={index} className="col-12 col-md-4 mb-4">
                   <div
                     className="card bg-grey shadow"
                     style={{ border: "none", width: "100%", maxWidth: "380px" }}
@@ -120,9 +188,9 @@ export default function LastSeasonsSection() {
                     </motion.div>
                   </div>
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </section>
